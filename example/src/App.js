6129ba2fe47980b143665133dcb72f1972c70d05
Akash Agarwal <akash191095@gmail.com>
2020-11-26 09:59:43 +0530

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'quick-react-notifications'
+import 'quick-react-notifications/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
